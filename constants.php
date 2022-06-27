412a8dd12917323f40ef3786981b3c67f4d0a50b
tuhin441 <61858202+tuhin441@users.noreply.github.com>
2022-06-27 13:10:05 +0600

    simple practices
    variables and constants

---
+++ constants.php
@@ -0,0 +1,17 @@
+<?php
+// define constants
+define ('PROGRAM', 'The Matrix');
+define ('VERSION', 11.7);
+// use constants
+// output: 'Welcome to The Matrix (version 11.7)'
+echo 'Welcome to ' . PROGRAM . ' (version ' . VERSION . ')';
+?>
+
+<?php 
+//define constants
+define ('PROGRAM1','The Matrix');
+define('VERSION1',11.7);
+//use constants
+//output: 'Welcome to the Matrix (version 11.7)'
+echo 'Welcome to' . PROGRAM . '(Version'. VERSION . ')'
+?>
---
+++ variable2.php
@@ -0,0 +1,25 @@
+<?php
+// define variables
+$p = 10;
+$q = 11;
+$r = 11.3;
+$s = 11;
+// test if $q is greater than $p
+// returns true
+echo ($q > $p);
+// test if $q is less than $p
+// returns false
+echo ($q < $p);
+// test if $q is greater than or equal to $s
+// returns true
+echo ($q >= $s);
+// test if $r is less than or equal to $s
+// returns false
+echo ($r <= $s);
+// test if $q is equal to $s
+// returns true
+echo ($q == $s);
+// test if $q is equal to $r
+// returns false
+echo ($q == $r);
+?>
