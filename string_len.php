ebcbfaf3c9907160eb88d5a37ea4b97479b93d91
tuhin441 <61858202+tuhin441@users.noreply.github.com>
2022-06-28 09:33:56 +0600

    string-manipulations

---
+++ string_len.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo strlen("Hello world!");
+?> 
+ 
+</body>
+</html>
+
+<!DOCTYPE html>
+<html>
+<body>
+
+    <?php
+    echo strlen("Hello world");
+    ?>
+
+</body>
+</html>
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo str_word_count("Hello world!");
+?> 
+ 
+</body>
+</html>
+
+<!DOCTYPE html>
+<html>
+    <body>
+        <?php
+        echo strrev("Hello World!");
+        ?>
+    </body>
+</html>
+
+<!DOCTYPE html>
+<html>
+    <body>
+        <?php
+        echo strpos("Hello World!, world" , "World!");
+        ?>
+    </body>
+</html>
+
+<!DOCTYPE html>
+<html>
+    <body>
+        <?php
+        echo str_replace("World", "Dolly" , "Hello World!");
+        ?>
+    </body>
+</html>
